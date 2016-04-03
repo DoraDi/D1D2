bd0eb493cd6b5e69dd948bc7e94a2bb02739884a
Dora Di <dodi@kea.dk>
2016-04-03 20:55:04 +0200

    D1 adds Controller

---
+++ D1D2/src/controller/Servlet.java
@@ -0,0 +1,68 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.corba.se.impl.protocol.giopmsgheaders.Message;
+
+/**
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet 
+{
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Servlet() 
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+	{
+		ServletContext context = request.getServletContext();
+		String user = request.getParameter("un");
+		String password = request.getParameter("pw");		
+        context.setAttribute("user",user); //attribute set for the whole context
+        
+        boolean isadmin = false;
+        String message;
+		
+        isadmin = control(user, password);
+        if (isadmin)
+        		message = "Welcome, ";
+        else	
+        		message = "You do not have permission to enter, ";
+    		response.getWriter().append(message + user);
+		
+		response.getWriter().append("/nControl executed by: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+	
+	boolean control(String a, String b)
+	{	
+		boolean c = false;
+		return c;
+	}
+
+}
